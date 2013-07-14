8eeffaf89936f0656a8a1764714256a50af0594a
Logan Campbell <colin@logaan.net>
2013-07-14 16:02:35 +1000

    lein generated

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lisp-c-compiler
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/lisp_c_compiler/core_test.clj
@@ -0,0 +1,7 @@
+(ns lisp-c-compiler.core-test
+  (:use clojure.test
+        lisp-c-compiler.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
